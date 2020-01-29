3342119da2af5cdfa8951d0b7362a4ddd87f442c
Matt <matthave@gmail.com>
2020-01-29 21:46:56 +0100

    Updates

---
+++ precache-manifest.757c33e26a73a3364ebac60a5f3d176f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e82a20211b97db0dc067d5ea1d06df",
+    "url": "/Kyokushin/index.html"
+  },
+  {
+    "revision": "5ec689f100deaa3765c4",
+    "url": "/Kyokushin/static/css/main.6e844020.chunk.css"
+  },
+  {
+    "revision": "0e9f20072a706ce18a3d",
+    "url": "/Kyokushin/static/js/2.e86cf9c3.chunk.js"
+  },
+  {
+    "revision": "f5ca32966347038a998bbf09dda684c7",
+    "url": "/Kyokushin/static/js/2.e86cf9c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5ec689f100deaa3765c4",
+    "url": "/Kyokushin/static/js/main.c2eeee78.chunk.js"
+  },
+  {
+    "revision": "3c76311cb85211e83603",
+    "url": "/Kyokushin/static/js/runtime-main.4a645228.js"
+  },
+  {
+    "revision": "980410773a8cb69902ac91e34c8060cd",
+    "url": "/Kyokushin/static/media/TreningiZdj6.98041077.jpg"
+  },
+  {
+    "revision": "20b58e7bbe2d4f01f1458f3724f6a848",
+    "url": "/Kyokushin/static/media/bg.20b58e7b.png"
+  },
+  {
+    "revision": "b74426a8bbedee0e70e5175ddef009be",
+    "url": "/Kyokushin/static/media/dojo.b74426a8.jpg"
+  },
+  {
+    "revision": "7a0328782fd4deb55e0aee60cb5e967e",
+    "url": "/Kyokushin/static/media/japan.7a032878.jpg"
+  },
+  {
+    "revision": "650de13404fb63e3ea932afc6f352910",
+    "url": "/Kyokushin/static/media/kyo.650de134.png"
+  },
+  {
+    "revision": "27f64bd184880621c69c8f8432965c3d",
+    "url": "/Kyokushin/static/media/kyoTxt.27f64bd1.gif"
+  },
+  {
+    "revision": "4ae74e0f541d2d7c5ee1c7b5f8973320",
+    "url": "/Kyokushin/static/media/sosai.4ae74e0f.jpg"
+  },
+  {
+    "revision": "0d484142e353d1ead841321792f3a13b",
+    "url": "/Kyokushin/static/media/symbol1.0d484142.jpg"
+  },
+  {
+    "revision": "693a80fdb080824fc6c959a5a577534b",
+    "url": "/Kyokushin/static/media/symbol2.693a80fd.jpg"
+  }
+]);
